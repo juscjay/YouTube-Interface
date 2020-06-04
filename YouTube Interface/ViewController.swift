46c904ca5b8cac55bc15885b27930415becac12f
CJ <juscjay@me.com>
2020-06-04 11:29:47 -0400

    initila xcode project creation

---
+++ YouTube Interface/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTube Interface
+//
+//  Created by juscjay on 6/4/20.
+//  Copyright © 2020 juscjay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
